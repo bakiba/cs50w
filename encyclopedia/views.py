8ce322d3efa932781ba7af2366941a1d4efdc4a0
Baki Ba <bakiba@gmail.com>
2021-01-31 20:03:43 +0100

    Implemented search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 
 from . import util
-
+import re
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    entries = util.list_entries()
+    query = request.GET.get('q', '')
+    if query != '':
+        page = util.get_entry(re.sub('\W+','', query))
+        if page is not None:
+            return render(request, "encyclopedia/page.html", {
+                "page": page,
+                "name": query
+            })
+        else:
+            found_entries = [ entry for entry in entries if query.lower() in entry.lower() ] 
+            return render(request, "encyclopedia/search.html", {
+                    "query": query,
+                    "entries":  found_entries
+            })    
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": entries
+        })
 
 def page(request, name):
     page = util.get_entry(name)
