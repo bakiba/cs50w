796feb9557fb137c093d631a69b0c74f2dd2d2b1
Baki Ba <bakiba@gmail.com>
2021-01-31 14:47:09 +0100

    Implemented entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,17 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def page(request, name):
+    page = util.get_entry(name)
+    if page is not None:
+        return render(request, "encyclopedia/page.html", {
+            "page": page,
+            "name": name
+        })
+    else:
+        return render(request, "encyclopedia/page.html", {
+            "page": "Page not found",
+            "name": name
+        }) 
+
+
