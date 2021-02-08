34f9077761fca718b1dec247debf9aafadb391a1
Baki Ba <bakiba@gmail.com>
2021-02-08 20:41:09 +0100

    Implemented markdown to html

--- encyclopedia/util.py
+++ encyclopedia/util.py
@@ -1,4 +1,5 @@
 import re
+import markdown2
 
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
@@ -19,10 +20,12 @@ def save_entry(title, content):
     content. If an existing entry with the same title already exists,
     it is replaced.
     """
+
     filename = f"entries/{title}.md"
     if default_storage.exists(filename):
         default_storage.delete(filename)
-    default_storage.save(filename, ContentFile(content))
+    # Fix for extra new lines added: https://stackoverflow.com/questions/62903909/django-contentfile-unexpected-empty-line-django-core-files-base
+    default_storage.save(filename, ContentFile(content.encode('ascii')))
 
 
 def get_entry(title):
@@ -35,3 +38,31 @@ def get_entry(title):
         return f.read().decode("utf-8")
     except FileNotFoundError:
         return None
+
+def to_html(content):
+    """
+    Simple implementation of Markdown to HTML using regex
+    Struggled with regex for paragraph, tried every stackowerflow regex... eventually gave off and used markdown2
+    https://stackoverflow.com/questions/51468095/tokenizing-consecutive-lines-as-paragraphs-in-markdown-with-regular-expressions
+    """
+    patterns = [
+        #('(?:\n{2,}|^)((?:(?![#>*\d.-]).+(?:\n|$))+)','<p>\n\\1</p>\n')
+        #('^[A-Za-z].*(?:\n[A-Za-z].*)*','<p>\\1</p>\n')
+        #('^(?![#>\-*\d ])((?![#>\-*\d ]).+\n?)+','<p>\n\\1</p>\n')
+        #('^#\s(.*)$','<h1>\\1</h1>\n'),
+        #('^##\s(.*)$','<h2>\\1</h2>\n')
+        ('(?P<para_all>(?:\n{2,}|^)(?P<para_prefix>[ ]{0,3})(?P<para_content>(?:(?![>*+-=\#]|\d+\.|\`{3,})).+(?!\n(?:[=-]+|\`{3,}))(?:\n|$))+)','<p>\\1</p>\n')
+    ]
+    html_content = content
+    for pattern, replacement in patterns:
+        html_content = re.sub(pattern, replacement, html_content, flags=re.MULTILINE)
+    
+    #print(content)
+    #print('=====================')
+    #print(html_content)
+    #print(markdown2.markdown(content))
+    #print('=====================')
+    #html_content += line
+    #return html_content
+    return markdown2.markdown(content)
+    
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -12,99 +12,135 @@ class NewPageForm(forms.Form):
     content = forms.CharField(label="Content", widget=forms.Textarea(attrs={'class': "form-control"}))
 
 def index(request):
+    # Rerieve all entries
     entries = util.list_entries()
+    # Did user submited search via GET?
     query = request.GET.get('q', '')
     if query != '':
+        # It looks like user wants to search for some page
+        # clean-up the query string from all non-word characters
         query = re.sub('\W+','', query)
+        # Search for all entries mathing query substring
         found_entries = [ entry for entry in entries if query.lower() in entry.lower() ] 
+        # We found only one entry and it is complete match, not just part of entry
+        # This is beucase if it is exact match, we should return page to user directly
         if len(found_entries) == 1 and query.lower() in (entry.lower() for entry in entries):
             page = util.get_entry(query)
             if page is not None:
                 return render(request, "encyclopedia/page.html", {
-                    "page": page,
+                    "page": util.to_html(page),
                     "name": query
                 })
         else:
+            # Show to search results
             return render(request, "encyclopedia/search.html", {
                     "query": query,
                     "entries":  found_entries
             })    
     else:
+        # We're not comming from search, so show list of all entries
         return render(request, "encyclopedia/index.html", {
             "entries": entries
         })
 
 def page(request, name):
+    # Get page from the disk
     page = util.get_entry(name)
     if page is not None:
+        # Return it to user
         return render(request, "encyclopedia/page.html", {
-            "page": page,
+            # Apply Markdown to HTML conversion
+            "page": util.to_html(page),
             "name": name
         })
     else:
+        # Return page not found in case we can't locate entry
         return render(request, "encyclopedia/page.html", {
             "page": "Page not found",
             "name": name
         }) 
 
 def newpage(request):
+    # We submitted create new page form
     if request.method == "POST":
+        # Get form data
         form = NewPageForm(request.POST)
         if form.is_valid():
-            # we will use slugify to create safe to write to disk
+            # we will use slugify to create safe title to write to disk
             title = slugify(form.cleaned_data["title"])
             content = form.cleaned_data["content"]
+            # If entry with same title exists, we will throw error
+            # Conver title to lower so we do not treat "CSS", "CSs", etc as separate entries
             if title != "" and title.lower() not in (entry.lower() for entry in util.list_entries()):
+                # All checks passed, save entry
                 util.save_entry(title, content)
+                # Show successfull message
                 messages.success(request, f'Page with title "{title}" successfully added.')
+                # Return the page to user in HTML format
                 return render(request, "encyclopedia/page.html", {
-                    "page": util.get_entry(title),
+                    "page": util.to_html(util.get_entry(title)),
                     "name": title
                 })
             else:
                 # we need to add "danger" tag to message to trigger red alert banner in bootstrap
                 DANGER = 50;
                 messages.add_message(request, DANGER, f'Page with title "{title}" already exists.', extra_tags='danger')
+                # Retrun form back to user
                 return render(request, "encyclopedia/newpage.html", {
                     "form": form,
                 })
     else:
+        # We're comming from "Create New Page"
         return render(request, "encyclopedia/newpage.html", {
             "form": NewPageForm()
         })
         
 def editpage(request, name):
+    # We're submitted the edite page via POST
     if request.method == "POST":
+        # Get form data from POST
         form = NewPageForm(request.POST)
         if form.is_valid():
+            # Get title and data
             title = form.cleaned_data["title"]
             content = form.cleaned_data["content"]
+            # We will only allow page data to be changed, so if user change title, it will not match the entry name
             if title == name:
+                # Title was not changed, save the entry and return the modified page to user
                 util.save_entry(title, content)
                 messages.success(request, f'Page with title "{title}" successfully edited.')
                 return render(request, "encyclopedia/page.html", {
-                    "page": util.get_entry(title),
+                    "page": util.to_html(util.get_entry(title)),
                     "name": title
                 })
             else:
                 # Title of the page was also changed, we do not have way of renaming the entry and it could get messy if we would allow to save as new page
                 DANGER = 50;
                 messages.add_message(request, DANGER, f'Can\'t change page title', extra_tags='danger')
+                # We subbmitted form with edite title and now must generate new form with original title and return it back to user
+                ret_form = NewPageForm({
+                    "title": name,
+                    "content": content
+                })
                 return render(request, "encyclopedia/editpage.html", {
-                    "form": form,
+                    "form": ret_form,
                 })
     else:
+        # We did not submit POST, this is when you click edit link on page
+        # Retrieve page
         page = util.get_entry(name)
         if page is not None:
+            # Populate the form
             form = NewPageForm({
                 "title": name,
                 "content": page
             })
-            
+            # Send it back to user
             return render(request, "encyclopedia/editpage.html", {
                 "form": form
             })
         else:
+            # Some error and we could not find requested page
             return render(request, "encyclopedia/page.html", {
                 "page": "Page not found",
                 "name": name
@@ -118,7 +154,7 @@ def randpage(request):
     # Render page
     if page is not None:
         return render(request, "encyclopedia/page.html", {
-            "page": page,
+            "page": util.to_html(page),
             "name": rand_title
         })
     else:
