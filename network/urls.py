ce8e34339c910b0c0f56fe1a7e7e4edd1bb0b93c
Baki Ba <bakiba@gmail.com>
2022-01-04 18:15:14 +0100

    Implemented following page

--- network/urls.py
+++ network/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path("posts", views.posts, name="posts"),
     path("like/<int:post_id>", views.like, name="like"),
     path("profile/<str:user>", views.profile, name="profile"),
-    path("follow/<str:user>", views.follow, name="follow")
+    path("follow/<str:user>", views.follow, name="follow"),
+    path("following", views.following, name="following")
 ]
