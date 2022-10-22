b4b6cbaa4492adea5554845d0a29347fc0e17e83
Baki Ba <bakiba@gmail.com>
2022-10-22 17:29:48 +0200

    Added template filters

---
+++ capstone/clientphotogallery/templatetags/app_filters.py
@@ -0,0 +1,17 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def asset_in_listdic(list, id):
+    if not any(d['asset_id'] == id for d in list):
+        return False
+    else:
+        return True
+
+@register.filter
+def get_print_count(list, id):
+    for item in list:
+        if item['asset_id'] == id:
+            return item['print_count']
+    return 1
